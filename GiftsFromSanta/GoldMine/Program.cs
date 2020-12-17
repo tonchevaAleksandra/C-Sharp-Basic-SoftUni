8961e7d493b54dbd361d49f472a88f58d5551708
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-12-17 16:01:55 +0200

    Add files

---
+++ GiftsFromSanta/GoldMine/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GoldMine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int locationsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < locationsCount; i++)
+            {
+                double expectedGoldYield = double.Parse(Console.ReadLine());
+                int daysCountOnLocation = int.Parse(Console.ReadLine());
+
+                double totalGoldForLocation = 0;
+                for (int j = 0; j < daysCountOnLocation; j++)
+                {
+                    double yieldForTheDay = double.Parse(Console.ReadLine());
+                    totalGoldForLocation += yieldForTheDay;
+                }
+
+                double averageYield = totalGoldForLocation / daysCountOnLocation;
+
+                if (averageYield >= expectedGoldYield)
+                { 
+                    Console.WriteLine($"Good job! Average gold per day: {averageYield:F2}.");
+                }
+
+                else
+                {
+                    double diffYieldPerDay = expectedGoldYield - averageYield;
+                    Console.WriteLine($"You need {diffYieldPerDay:f2} gold.");
+                }
+            }
+        }
+    }
+}
---
+++ GiftsFromSanta/PuppyCare/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PuppyCare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodInKg = int.Parse(Console.ReadLine());
+
+            int foodInGrams = foodInKg * 1000;
+            int totalEatedFood = 0;
+
+            string command = Console.ReadLine();
+            while (command != "Adopted")
+            {
+                int foodEatedGrams = int.Parse(command);
+                totalEatedFood += foodEatedGrams;
+
+                command = Console.ReadLine();
+            }
+
+            if (foodInGrams >= totalEatedFood)
+            {
+                int foodLeft = foodInGrams - totalEatedFood;
+                Console.WriteLine($"Food is enough! Leftovers: {foodLeft} grams.");
+            }
+            else
+            {
+                int missingQuantityFood = totalEatedFood - foodInGrams;
+                Console.WriteLine($"Food is not enough. You need {missingQuantityFood} grams more.");
+            }
+        }
+    }
+}
